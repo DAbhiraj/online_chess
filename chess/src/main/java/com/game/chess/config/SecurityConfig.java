d76107c204159f265cf44bc7816bac456cdef2d9
[abhiraj] <abhiraj.dustakar@gmail.com>
2025-06-15 20:31:07 +0530

    updated origin patterns

--- chess/src/main/java/com/game/chess/config/SecurityConfig.java
+++ chess/src/main/java/com/game/chess/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://sweet-bublanina-8cd739.netlify.app")); 
+        configuration.setAllowedOriginPatterns(Arrays.asList("https://*.netlify.app")); 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
--- chess/src/main/java/com/game/chess/controller/AuthController.java
+++ chess/src/main/java/com/game/chess/controller/AuthController.java
@@ -25,7 +25,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "https://sweet-bublanina-8cd739.netlify.app")
 @Slf4j
 public class AuthController {
 
